2606ff9b68ae447583af7dbd31b60fcd2d8e5dac
Darian <2120651@swansea.ac.uk>
2024-03-04 18:27:50 +0000

    Tried updating the table to account for collisions

--- transpositionTable.js
+++ transpositionTable.js
@@ -15,10 +15,10 @@ class TranspositionTable {
      */
     constructor() {
         /**
-         * @type {Object[]} the table 
+         * @type {Object[][]} the table 
          */
         this.table = new Array(this.totalSize);
-        this.table.fill(null);
+        this.table.fill([]);
     }
 
 
@@ -47,13 +47,28 @@ class TranspositionTable {
      * @param {number} depthSearched the depth the game state was searched to
      */
     add(gameState, value, depthSearched) {
-        var index = this.getIndexOfKey(this.hashGameState(gameState));
+        var myHash = this.hashGameState(gameState);
+        var index = this.getIndexOfKey(myHash);
         //if the index is empty, add the game state
-        //if depthSearched is greater than the depthSearched in the table, update the table as the result is more accurate
-        if(this.table[index] == null){
-            this.table[index] = {value: value, depthSearched: depthSearched};
-        } else if(this.table[index].depthSearched < depthSearched) {
-            this.table[index] = {value: value, depthSearched: depthSearched};
+        if(this.table[index] == []){
+            this.table[index].push({value: value, hash: myHash, depthSearched: depthSearched});
+        } else {
+            var found = false;
+            for(var i = 0; i < this.table[index].length; i++){
+                if(this.table[index][i].hash == myHash){
+                    //if depthSearched is greater than the depthSearched in the table, update the table as the result is more accurate
+                    if(this.table[index][i].depthSearched < depthSearched){
+                        this.table[index][i] = {value: value, hash: myHash, depthSearched: depthSearched};
+                    }
+                    found = true;
+                    return;
+                }
+            }
+            //if the game state was not found in the table, add it
+            if(!found){
+                this.table[index].push({value: value, hash: myHash, depthSearched: depthSearched});
+            }
+
         }
     }
 
@@ -64,16 +79,25 @@ class TranspositionTable {
      * @returns {number} the value of the game state
      */
     get(gameState, currentDepth) {
-        var index = this.getIndexOfKey(this.hashGameState(gameState));
-        if(this.table[index] == null){
+        var myHash = this.hashGameState(gameState);
+        var index = this.getIndexOfKey(myHash);
+        //nothing at the index
+        if(this.table[index] == []){
             return null;
         }
-        //we only want to return the value if the depthSearched is greater than or equal to the current depth otherwise the result is not accurate
-        if(this.table[index].depthSearched >= currentDepth){
-            return this.table[index].value;
-        } else {
-            return null;
+        //something is at the index so search through the array
+        for(var i = 0; i < this.table[index].length; i++){
+            if(this.table[index][i].hash == myHash){
+                //we only want to return the value if the depthSearched is greater than or equal to the current depth otherwise the result is not accurate
+                if(this.table[index][i].depthSearched >= currentDepth){
+                    return this.table[index][i].value;
+                } else {
+                    return null;
+                }
+            }
         }
+        //nothing was found
+        return null;
     }
     
 }
